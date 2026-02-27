41a93a242ad7378b84fa671bc6ec532cd104fe75
cleitonsetti-debug <cleiton_setti@estudante.sesisenai.org.br>
2026-02-27 19:43:55 +0000

    update

---
+++ atividade 1/programa3.py
@@ -0,0 +1,5 @@
+nome = input("insira o seu nome")
+numero1 = int(input("insira o primeiro numero"))
+numero2 = int(input("insira o segundo numero"))
+print("o usuario ",nome," obteve a soma dos dois valores de :",numero1 + numero2)
+#tem que colocar o int para somar por que senao ele entende como caracter
