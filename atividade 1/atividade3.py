28c3fd449a9a0353e93215588292ed92a28b04bc
cleitonsetti-debug <cleiton_setti@estudante.sesisenai.org.br>
2026-04-10 19:29:01 +0000

    update

---
+++ atividade 1/atividade3.py
@@ -0,0 +1,10 @@
+import time
+
+contador = 10
+
+while contador >=0:
+     print("iniciando secagem:", contador, "segundos")
+        time.sleep(1)
+        contador = contador - 1
+
+     print("secagem cuncluida a porta pode ser aberta")
