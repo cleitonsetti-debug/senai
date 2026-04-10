7dd2879c0281182fb9a2f20d9636cc9081dc1076
cleitonsetti-debug <cleiton_setti@estudante.sesisenai.org.br>
2026-04-10 18:30:43 +0000

    update

--- atividade 1/desafio2.py
+++ atividade 1/desafio2.py
@@ -1,8 +1,12 @@
 contador = 1
 soma = 0
 while contador <= 5:
+    # Solicita o salário ao usuário e converte para número decimal (float)
     salario = float(input(f"digite o salario do { contador } funcionario: "))
+    # Incrementa o contador para evitar um loop infinito e passar para o próximo funcionário
     contador += 1
+
     soma += salario
+    # Após o fim do loop, calcula a média aritmética
 media = soma / 5
-print("a media dos salariois dos 5 funcionarios e", media)
+print("a media dos salariois dos 5 funcionarios é:", media)
