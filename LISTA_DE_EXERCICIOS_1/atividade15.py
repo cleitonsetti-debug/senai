84f0170c0950520d1266d49b94d12bb20d1f3daf
cleitonsetti-debug <cleiton_setti@estudante.sesisenai.org.br>
2026-04-24 19:47:57 +0000

    update

---
+++ LISTA_DE_EXERCICIOS_1/atividade15.py
@@ -0,0 +1,24 @@
+total = float(input("digite o valor da compra: "))
+valor1 = total//3
+felipe = total - (2* valor1)
+
+print("valor de carlos é :", valor1)
+print("valor de andre é :", valor1)
+print("valor de felipe é :", felipe)
+
+
+#2 maneira de resolver
+
+# Ler o valor total da conta
+#valor_total = float(input("Digite o valor total da conta: "))
+# Dividir o valor total igualmente entre os três amigos
+#valor_por_pessoa = valor_total / 3
+# Calcular quanto cada um deve pagar
+#carlos = int(valor_por_pessoa)
+#andre = int(valor_por_pessoa)
+#felipe = valor_total - (carlos + andre)
+
+# Exibir quanto cada um deve pagar
+#print("Carlos deve pagar R$ {:.2f}".format(carlos))
+#print("André deve pagar R$ {:.2f}".format(andre))
+#print("Felipe deve pagar R$ {:.2f}".format(felipe))"
