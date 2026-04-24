006b7073261e32741c6e210ea8bb8042e7f5f7c6
cleitonsetti-debug <cleiton_setti@estudante.sesisenai.org.br>
2026-04-24 20:17:14 +0000

    update

---
+++ LISTA_DE_EXERCICIOS_1/atividade18.py
@@ -0,0 +1,6 @@
+hora_normais = float(input("digite a quantidade de horas normais: "))
+hora_extras = float(input("digite a quantidade de horas extras normais: "))
+
+valor_horas_normais = hora_normais * 10
+valor_horas_extras = hora_extras * 15
+
---
+++ LISTA_DE_EXERCICIOS_1/atividade19.py
@@ -0,0 +1,6 @@
+quanti_frangos = int(input("digite a quantidade de frangos: "))
+
+custo_chip = 4.0
+custo_alimento = 3.50
+
+gasto_por_frango = custo_chip * (custo_chip + custo_alimento)
