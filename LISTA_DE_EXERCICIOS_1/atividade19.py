71c8383c35ea468dc16a4b40ee7f53a2ee51be2a
cleitonsetti-debug <cleiton_setti@estudante.sesisenai.org.br>
2026-05-08 17:19:12 +0000

    update

--- LISTA_DE_EXERCICIOS_1/atividade19.py
+++ LISTA_DE_EXERCICIOS_1/atividade19.py
@@ -1,6 +1,13 @@
 quanti_frangos = int(input("digite a quantidade de frangos: "))
 
+
 custo_chip = 4.0
 custo_alimento = 3.50
 
-gasto_por_frango = custo_chip * (custo_chip + custo_alimento)
+gasto_por_frango = custo_chip + (2 * custo_alimento)
+
+gasto_total = quati_frangos * gasto_por_frango
+
+print("aquantia de frangos no aviario ou na fazenda é:", )
+
+print("para colocar a quantia dezejada de aneis sera presiso gastar: ",gasto_por_frango)
---
+++ LISTA_DE_EXERCICIOS_1/atividade20.py
@@ -0,0 +1,5 @@
+quanti_blusas = int(input("digite a quantidade de blusas necessarias: "))
+metros_totais = quanti_blusas * 120
+novelos = metros_totais // 125
+
+print(f"total de novelos necessarios: {novelos} ")
