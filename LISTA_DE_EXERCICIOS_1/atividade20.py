9c2ceccac3ab1125475cb49589bbdef5bee16df0
cleitonsetti-debug <cleiton_setti@estudante.sesisenai.org.br>
2026-05-08 17:28:09 +0000

    update

--- LISTA_DE_EXERCICIOS_1/atividade20.py
+++ LISTA_DE_EXERCICIOS_1/atividade20.py
@@ -2,4 +2,7 @@ quanti_blusas = int(input("digite a quantidade de blusas necessarias: "))
 metros_totais = quanti_blusas * 120
 novelos = metros_totais // 125
 
+if metros_totais % 125 > 0:
+        novelos += 1
+
 print(f"total de novelos necessarios: {novelos} ")
