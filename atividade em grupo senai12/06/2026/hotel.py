5691151e45f5612c5dab3451945a74e1509f9c49
cleitonsetti-debug <cleiton_setti@estudante.sesisenai.org.br>
2026-06-12 20:06:11 +0000

    update

--- atividade em grupo senai12/06/2026/hotel.py
+++ atividade em grupo senai12/06/2026/hotel.py
@@ -23,8 +23,10 @@ while True:
         if dias > 7:
             print("Parabéns! Você ganhou R$ 100.00 de desconto por longa estadia.")
             valor_desconto = valor_total - 100.00
-            
-        print(f"O valor total da sua reserva é: R$ {valor_desconto:.2f}")
+            print(f"O valor total da sua reserva com desconto é: R$ {valor_desconto:.2f}")
+            break
+        
+        print(f"o valor da sua reserva é: r$",valor_total)
         
     elif opcao == "2":
         print("Obrigado pelo interesse!")
